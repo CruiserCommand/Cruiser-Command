41d08cedf1cbcecbadde0c5362baa6dd7b628107
Siretu <erikihr@gmail.com>
2013-07-06 12:02:40 +0200

    Fixed deselection bug. RTSUnitSelection.cs
    Fixed a deselction bug that occured when there were several units on the screen at the same time. Also added support for shift-selection.

--- Cruiser-Command/Assets/Scripts/RTSSelectableUnit.cs
+++ Cruiser-Command/Assets/Scripts/RTSSelectableUnit.cs
@@ -1,8 +1,8 @@
 /*
  * Name: RTS Unit Selection
- * Author: James 'Sevion' Nhan
- * Date: 03/07/2013
- * Version: 1.0.1.0
+ * Author: James 'Sevion' Nhan and Erik 'Siretu' Ihren
+ * Date: 06/07/2013
+ * Version: 1.0.2.0
  * Description:
  * 		This is a simple RTS movement script that handles
  * 		unit selection.
@@ -12,7 +12,6 @@ using UnityEngine;
 using System.Collections;
 
 public class RTSSelectableUnit : MonoBehaviour {
-    public static int HighlightedUnits = 0;
     public GameObject SelectionCircle = null;
     private GameObject SelectionCircleInstance = null;
     private RTSUnitSelectionManager UnitManager;
@@ -24,46 +23,55 @@ public class RTSSelectableUnit : MonoBehaviour {
 
     void OnMouseUpAsButton() {
         // If the Selection Circle doesn't exist, create it at the unit
+		Debug.Log ("Foo");
         if (SelectionCircleInstance == null) {
             SelectionCircleInstance = GameObject.Instantiate(SelectionCircle, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - gameObject.transform.localScale.y / 2 + 0.01f, gameObject.transform.position.z), Quaternion.identity) as GameObject;
             // Parent it to the unit so it follows it
             SelectionCircleInstance.transform.parent = gameObject.transform;
+			Debug.Log(SelectionCircleInstance.transform.position);
+			Debug.Log(gameObject.transform.position);
         }
         // Select the unit
-        UnitManager.SelectUnit(gameObject);
+		if(!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.RightShift)){
+			UnitManager.ClearSelection();
+			UnitManager.SelectUnit(gameObject);
+		} else {
+			if(UnitManager.IsSelected(gameObject)){
+				UnitManager.DeselectUnit(gameObject);
+			} else {
+        		UnitManager.SelectUnit(gameObject);
+			}
+		}
     }
 
     void OnTriggerEnter(Collider col) {
+		Debug.Log("In OnTriggerEnter");
         // This keeps track of the number of units currently highlighted
-        HighlightedUnits++;
+		UnitManager.HighlightUnit(gameObject);
         // If the Selection Circle doesn't exist, create it at the unit
         if (SelectionCircleInstance == null) {
+			Debug.Log("Created circle");
             SelectionCircleInstance = GameObject.Instantiate(SelectionCircle, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - gameObject.transform.localScale.y / 2 + 0.01f, gameObject.transform.position.z), Quaternion.identity) as GameObject;
             // Parent it to the unit so it follows it
             SelectionCircleInstance.transform.parent = gameObject.transform;
+			Debug.Log(SelectionCircleInstance.transform.position);
+			Debug.Log(gameObject.transform.position);
         }
 	}
 
     void OnTriggerExit(Collider col) {
         // This unit left the Selection Box, so decrement the number of highlighted units
-        HighlightedUnits--;
+        UnitManager.UnhighlightUnit(gameObject);
         // Then destroy the Selection Circle
+		Debug.Log("Destroyed circle");
+		Debug.Log(SelectionCircleInstance);
         GameObject.DestroyObject(SelectionCircleInstance);
 	}
-
-    void Update() {
-        // Check if we have no units selected when we let go of the mouse
-        if (RTSUnitSelection.Unselect) {
-            // None, so deselect all units
-            GameObject.DestroyObject(SelectionCircleInstance);
-            // Reset Untselect so we don't have rubber ducking (Gogo Erik)
-            RTSUnitSelection.Unselect = false;
-        }
-        // Select the unit if it's highlighted else deselect it
-        if (SelectionCircleInstance != null) {
-            UnitManager.SelectUnit(gameObject);
-        } else {
-            UnitManager.DeselectUnit(gameObject);
-        }
-    }
+	
+	void Update(){
+		if(!UnitManager.IsSelected(gameObject) && !UnitManager.IsHighlighted(gameObject)){
+			Destroy(SelectionCircleInstance);
+			SelectionCircleInstance = null;
+		}
+	}
 }
