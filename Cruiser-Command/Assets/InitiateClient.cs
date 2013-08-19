8b5bde7fed0cee1ec63b1515f887b7c20010bcca
Siretu <erikihr@gmail.com>
2013-08-19 15:46:23 +0200

    Started working on basic networking

---
+++ Cruiser-Command/Assets/InitiateClient.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitiateClient : MonoBehaviour {
+    public string serverAddress = "127.0.0.1";
+    public int serverPort = 5666;
+
+	// Use this for initialization
+	void Start () {
+
+        // Make it possible to use UDP using a random port
+        //TNManager.StartUDP(Random.Range(10000, 50000));
+
+        // Connect to the remote server
+        //TNManager.Connect(serverAddress, serverPort);
+
+        TNManager.Connect("127.0.0.1:5668");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnNetworkConnect(bool success, string message) {
+        Debug.Log("Connected!");
+    }
+}
---
+++ Cruiser-Command/Assets/Scripts/InitiateServer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitiateServer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        TNServerInstance.Start(5666);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
