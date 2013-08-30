4850c76f793ac92b073636a9d32c301c98bff92a
Siretu <erikihr@gmail.com>
2013-08-30 16:30:58 +0200

    Adding some additional files

---
+++ Cruiser-Command/Assets/Scripts/Consoles/Resources/PowerConsole.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using CC.eLog;
+
+public class PowerConsole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void EnterConsole(GameObject obj) {
+        Log.Trace("console", "Entered power console circle");
+    }
+}
